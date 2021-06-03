50a6c5f1eb434091156aea4a10dc109fa1bfbcea
Kevin <Pea>
2021-06-03 16:53:09 -0600

    Cambios, modulos, Tipo de pago,Sexo,Ubicacion
    oki

--- application/views/Sexo/sexo_add.php
+++ application/views/Sexo/sexo_add.php
@@ -15,60 +15,10 @@ else{ //caso contrario, dejar las variables vacias por que significa nuevo regis
 	$accion           = 'nuevoSexo';
 }
 ?>
-<style type="text/css">
-	.bi-file-text-fill,.bi-plus-circle{
-		color: black;
-	}
+<?php $this->load->view('other'); ?>
+<! ––Carga la vista del Menu de administrador ––>
 </style>
 <div class="container">
-	<div class="row">
-		<div class="col">
-			<h1 class="panel">Panel de administración
-				<small class="tittles-pages-logo">STORE ONLINE</small>
-			</h1>
-		</div>
-	</div>
-	<hr>
-	<div class="row">
-		<div class="col">
-
-			<nav class="nav" style="background-color: #0A0C57;border-radius: 10px;">
-				<a class="nav-link active" href="<?php echo base_url(); ?>">
-					<i class="bi bi-person-fill" style="font-size: 25px;"></i>
-					Usuarios
-				</a>
-				<a class="nav-link active" href="<?php echo base_url(); ?>SexoController">
-					<i class="bi bi-people-fill" style="font-size: 25px;"></i>
-					Sexo
-				</a>
-				<a class="nav-link active" href="<?php echo base_url(); ?>">
-					<i class="bi bi-file-earmark-lock-fill" style="font-size: 25px;"></i>
-					Roles
-				</a>
-				<a class="nav-link active" href="<?php echo base_url(); ?>Categoria/Index">
-					<i class="bi bi-book-half" style="font-size: 25px;"></i>
-					Categorias
-				</a>
-				<a class="nav-link active" href="<?php echo base_url(); ?>Catalogo/CatalogoAdmin">
-					<i class="bi bi-camera-fill" style="font-size: 25px;"></i>
-					Productos
-				</a>
-				<a class="nav-link active" href="<?php echo base_url(); ?>Proveedores/Index">
-					<i class="bi bi-truck" style="font-size: 25px;"></i>
-					Proveedores
-				</a>
-				<a class="nav-link active" href="<?php echo base_url(); ?>tipoPagoController/">
-					<i class="bi bi-credit-card" style="font-size: 25px;"></i>
-					Pagos
-				</a>
-				<a class="nav-link active" href="<?php echo base_url(); ?>ubicacionController/">
-					<i class="bi bi-geo-alt-fill" style="font-size: 25px;"></i>
-					Ubicación
-				</a>
-			</nav>
-
-		</div>
-	</div>
 	<!-- Navegacion del mantenimiento -->
 	<div class="row">
 		<div class="col">
