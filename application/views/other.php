c2bf7ae3b14b09b33cc12b8b60bbaa87b54d24fe
yasmin0026 <rhina.espinoza201099@liveusam.edu.sv>
2021-06-03 15:36:20 -0600

    Cambio

--- application/views/other.php
+++ application/views/other.php
@@ -10,49 +10,46 @@
 	</div>
 	<hr>
 	
-		<div class="row">
-			<div class="col">
+	<div class="row">
+		<div class="col">
 
-				<nav class="nav" style="background-color: #0A0C57;border-radius: 10px;">
-					<a class="nav-link active" href="<?php echo base_url(); ?>Usuario">
-						<i class="bi bi-person-fill" style="font-size: 25px;"></i>
-						Usuarios
-					</a>
-					<a class="nav-link active" href="<?php echo base_url(); ?>SexoController">
-						<i class="bi bi-people-fill" style="font-size: 25px;"></i>
-						Sexo
-					</a>
-					<a class="nav-link active" href="<?php echo base_url(); ?>Roles/index">
-						<i class="bi bi-file-earmark-lock-fill" style="font-size: 25px;"></i>
-						Roles
-					</a>
-					<a class="nav-link active" href="<?php echo base_url(); ?>Categoria/Index">
-						<i class="bi bi-book-half" style="font-size: 25px;"></i>
-						Categorias
-					</a>
-					<a class="nav-link active" href="<?php echo base_url(); ?>Catalogo/CatalogoAdmin">
-						<i class="bi bi-camera-fill" style="font-size: 25px;"></i>
-						Productos
-					</a>
-					<a class="nav-link active" href="<?php echo base_url(); ?>Proveedores/Index">
-						<i class="bi bi-truck" style="font-size: 25px;"></i>
-						Proveedores
-					</a>
-					<a class="nav-link active" href="<?php echo base_url(); ?>tipoPagoController/">
-						<i class="bi bi-credit-card" style="font-size: 25px;"></i>
-						Pagos
-					</a>
-					<a class="nav-link active" href="<?php echo base_url(); ?>ubicacionController/">
-						<i class="bi bi-geo-alt-fill" style="font-size: 25px;"></i>
-						Ubicación
-					</a>
-				</nav>
-				
-			</div>
+			<nav class="nav" style="background-color: #0A0C57;border-radius: 10px;">
+				<a class="nav-link active" href="<?php echo base_url(); ?>Usuario">
+					<i class="bi bi-person-fill" style="font-size: 25px;"></i>
+					Usuarios
+				</a>
+				<a class="nav-link active" href="<?php echo base_url(); ?>SexoController">
+					<i class="bi bi-people-fill" style="font-size: 25px;"></i>
+					Sexo
+				</a>
+				<a class="nav-link active" href="<?php echo base_url(); ?>Roles/index">
+					<i class="bi bi-file-earmark-lock-fill" style="font-size: 25px;"></i>
+					Roles
+				</a>
+				<a class="nav-link active" href="<?php echo base_url(); ?>Categoria/Index">
+					<i class="bi bi-book-half" style="font-size: 25px;"></i>
+					Categorias
+				</a>
+				<a class="nav-link active" href="<?php echo base_url(); ?>Catalogo/CatalogoAdmin">
+					<i class="bi bi-camera-fill" style="font-size: 25px;"></i>
+					Productos
+				</a>
+				<a class="nav-link active" href="<?php echo base_url(); ?>Proveedores/Index">
+					<i class="bi bi-truck" style="font-size: 25px;"></i>
+					Proveedores
+				</a>
+				<a class="nav-link active" href="<?php echo base_url(); ?>tipoPagoController/">
+					<i class="bi bi-credit-card" style="font-size: 25px;"></i>
+					Pagos
+				</a>
+				<a class="nav-link active" href="<?php echo base_url(); ?>ubicacionController/">
+					<i class="bi bi-geo-alt-fill" style="font-size: 25px;"></i>
+					Ubicación
+				</a>
+			</nav>
+
 		</div>
-		
-
 	</div>
-
+</div>
 
 <br>
